597e26a017662f2236a2fb6622c5b33cfedc4d11
Daisha <daishadeniz@gmail.com>
2023-05-15 16:18:58 -0400

    tested get request and it works

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
-
+const routes = require('./routes');
 const app = express();
 const PORT = 3001;
+
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log('server listening on port 3001');

068e226cbd3181dd1ec200645cb8cd7e18197c10
Daisha <daishadeniz@gmail.com>
2023-05-16 12:44:43 -0400

    working on login, pulling down updated code

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-    console.log('server listening on port 3001');
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ });
+
+
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });

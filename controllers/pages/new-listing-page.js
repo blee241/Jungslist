041e6ded979c80279ccacb02aed72f71e2a5372d
blee241 <128437170+blee241@users.noreply.github.com>
2023-05-16 09:59:08 -0700

    created clearer structures for routing the pages

--- controllers/pages/new-listing-page.js
+++ controllers/pages/new-listing-page.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.json('From seller-page file')
+    res.json('From new-listing-page file')
 })
 
 module.exports = router;

a71a15d1fbb2af4924719b0e6b77b9801a799b6f
Daisha <daishadeniz@gmail.com>
2023-05-17 14:07:19 -0400

    added index.js in connection back

---
+++ controllers/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const pages = require('./pages');
+
+
+const apiRoutes = require('./api');
+
+
+router.use('/api', apiRoutes);
+
+// localhost:3001/
+router.use('/', pages);
+
+
+
+module.exports = router;

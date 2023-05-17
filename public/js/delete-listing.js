0b82e3beb19975f3d5b2f0ab4431a537b092bc19
blee241 <128437170+blee241@users.noreply.github.com>
2023-05-17 12:29:38 -0700

    the delete button removes the listing element from the client side and destroys the corresponding listing table in the database

--- public/js/delete-listing.js
+++ public/js/delete-listing.js
@@ -1,22 +1,23 @@
-let deleteListingBtnEl = document.querySelector('.delete-button');
-
-const deleteListing = async () => {
-    const listingId = deleteListingBtnEl.dataset.listingId;
-    try {
-      const response = await fetch(`/delete_listing/${listingId}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        alert('You have successfully deleted a listing!');
-        const section = document.getElementById(listingId);
-        section.remove();
-      } else {
-        alert('Something went wrong');
-      }
-    } catch (error) {
-      console.error('Error occurred during delete operation', error);
+let deleteListingBtns = document.querySelectorAll('.delete-button');
+const deleteListing = async (listingId) => {
+  try {
+    const response = await fetch(`/sell/delete_listing/${listingId}`, {
+      method: 'DELETE',
+    });
+    if (response.ok) {
+      alert('You have successfully deleted a listing!');
+      const section = document.getElementById(listingId);
+      section.remove();
+    } else {
+      alert('Something went wrong');
     }
-  };
-  
-  deleteListingBtnEl.addEventListener('click', deleteListing);
+  } catch (error) {
+    console.error('Error occurred during delete operation', error);
+  }
+};
+deleteListingBtns.forEach((btn) => {
+  btn.addEventListener('click', (event) => {
+    const listingId = event.target.dataset.listingId;
+    deleteListing(listingId);
+  });
+});
